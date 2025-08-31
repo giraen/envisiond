11e65a59d7b992e6de3574de15de8ab4989a87d3
giraen <122036189+giraen@users.noreply.github.com>
2025-09-01 02:14:37 +0800

    New Implementations FEAT: Improved layout in login for all views FEAT: Added a global navbar except for logins FEAT: Navbar is available for all views

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -14,12 +14,10 @@ export default function Login() {
         console.log("Password:", password);
     }
 
-    return (
-        <>
-            <div className="grid grid-cols-1 gap-4 [&>*:last-child]:mt-0 mx-8 my-20 px-8 py-4 md:mx-42 lg:mx-124 rounded-3xl bg-[var(--color-foreground)] text-[var(--background)]">
-                <div
-                    className="flex justify-center"
-                >
+    return(
+        <div className="flex flex-col justify-center h-[75vh] px-8 md:px-28 lg:px-96 text-[var(--background)]">
+            <div className="grid grid-cols-1 gap-4 lg:gap-8 rounded-xl py-8 bg-[var(--color-foreground)]">
+                <div className="row-start-1 flex justify-center">
                     <Image
                         src="/logo_with_name.jpg"
                         alt="Logo"
@@ -28,57 +26,51 @@ export default function Login() {
                     />
                 </div>
 
-                <h1 className="font-bold text-center md:text-2xl">Log in to your account</h1>
+                <h1 className="font-bold text-center text-xl">Log in to your account</h1>
 
-                <form 
+                <form
                     onSubmit={handleSubmit}
-                    className="flex flex-col gap-4 flex-wrap"
+                    className="flex flex-col gap-4 mx-auto px-6 w-full max-w-md md:max-w-lg lg:max-w-xl"
                 >
-                    <div
-                        className="md:flex gap-16 items-center"
-                    >
-                        <label htmlFor="inputEmail">Email: </label>
+                    <div className="flex flex-col gap-1 md:flex-row md:items-center md:gap-4">
+                        <label htmlFor="inputEmail" className="text-sm text-gray-600 md:w-24 md:text-right">Email: </label>
                         <input 
                             id="inputEmail"
                             name="inputEmail"
                             type="email" 
                             value={email}
+                            placeholder="juandelacruz@email.com"
                             onChange={(e) => setEmail(e.target.value)}
                             required
-                            className="border w-full px-4 py-2 rounded"
+                            className="border border-gray-400 w-full px-3 py-2 rounded text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
                         />
                     </div>
-
-                    <div
-                        className="md:flex gap-9 items-center"
-                    >
-                        <label htmlFor="inputPassword">Password: </label>
+                    <div className="flex flex-col gap-1 md:flex-row md:items-center md:gap-4">
+                        <label htmlFor="inputPassword" className="text-sm text-gray-600 md:w-24 md:text-right">Password: </label>
                         <input 
                             id="inputPassword"
                             name="inputPassword"
                             type="password" 
                             value={password}
+                            placeholder="********"
                             onChange={(e) => setPassword(e.target.value)}
                             required
-                            className="border w-full px-4 py-2 rounded"
+                            className="border border-gray-400 w-full px-3 py-2 rounded text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
                         />
                     </div>
-
                     <div
                         className="flex flex-col text-center"
                     >
                         <button
                             type="submit"
-                            className="bg-blue-700 px-16 py-1 md:px-28 font-bold rounded-2xl text-white self-center hover:bg-blue-900 transition duration-300 ease-in-out"
+                            className="bg-blue-700 px-20 py-2 md:px-28 font-bold rounded-3xl text-white self-center hover:bg-blue-900 transition duration-300 ease-in-out"
                         >
                             Login
                         </button>
                         <Link href="#" className="no-underline mt-2 text-xs text-gray-600 hover:text-gray-900 transition duration-300 ease-in-out">Forgot Password?</Link>
                     </div>
-                    
                 </form>
             </div>
-        </>
-        
+        </div>
     )
 }
